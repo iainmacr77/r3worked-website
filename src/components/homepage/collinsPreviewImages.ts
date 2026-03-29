d3dee80407742d0a7bbdbe6143cb0a6db07328d1
Iain MacRitchie <im@liberty-wealth.com>
2026-03-29 15:55:58 +0200

    looking rockstar

--- src/components/homepage/collinsPreviewImages.ts
+++ src/components/homepage/collinsPreviewImages.ts
@@ -22,11 +22,11 @@ export const collinsAfterPreviewImagePaths: CollinsAfterPreviewImages = {
   hero: "/images/collins/after/collins-after-hero-2520x1260.webp",
   craftsmanship: "/images/collins/after/collins-after-craftsmanship-1800x1200.webp",
   projectGlassPavilion:
-    "https://lh3.googleusercontent.com/aida-public/AB6AXuBlOmDMyT0rLWniBeACoQPAXO3bC2klD9tTSkQDGb44bMg-SaDQBsAoVxyIWTQ97tyMfbntoNCbkeqOZHuHMzkLmDklPzeTRIgRQSJLR-IvcGa7fR1jh8zpXuRID8Zod5hQALDXnSUcStYwAyG59laXKkQiZHiOKf5Q-M_YQLwQt-V0lEH0w97JWrAYdbDhwO6eVXIPYHcuCBa1V3Nq7wNLrWj6xvAxKJaP_-Mmic2--vM2QwBeDJmKK4y3tNvBujOth0hrMgFlVIK1",
+    "/images/collins/after/collins-after-project-glass-pavilion-2000x1000.webp",
   projectVictorian:
-    "https://lh3.googleusercontent.com/aida-public/AB6AXuCzY_kZaVyhDy4zLcuai23KaWe1CxUD7fEIM9d23T9EpUFHgXV4xWjbUS4wEvUJ-J3dXWjnuwiV-0K36wScOW6J7ZIO9Aj7o9kwvEGgn2finiRXXTLkbxQFuqh7HRigGZxmAr65CfDbORm-Hvx1JIvZzur4opi_hjKFu7JwN-t9DkJ_pvfu3ygqdBAvPN3Ku9K5YCVNyarmiK-dBiCxPRk9GFiQUMKgpKPsFl9UjLqdvc19TP4Swqt_RWtzKYDKz0D5EPFaeG9GzPEO",
+    "/images/collins/after/collins-after-project-victorian-1400x1400.webp",
   projectModernist:
-    "https://lh3.googleusercontent.com/aida-public/AB6AXuCjsRzZsXiFhi-mxrIaIns6eXVMzjYw1R-fRC9UygaQRm5HODRRJL1sn_xjbt0crU8HSigNfIm10mGqFDeze1IWveP3hzYFpx2968Hsd3KviVtE00oFiLDtOaA_KH-UGFroGjgH9sWIfpBnT9IHOjAvgbih3hVzteZ2IyG1TaM5NRvJiQ7JcISMGWZrL3NCtO0QQpY4tq6xQ6xORUt1uPpBVzYwtDgTrdXJVIAidz7Ukq6Vy7aMNuM4T8ekfh1Upe5YcjSs2HcHzgb3",
+    "/images/collins/after/collins-after-project-modernist-1400x1400.webp",
   projectOpenPlan:
-    "https://lh3.googleusercontent.com/aida-public/AB6AXuBDM1YA7l0DUO3Jwx54VN5nXuAXd0jo2d4qHWJz9wywIg8VrtA62q3iAxTuEdchPCrXNRWFJ01G98xxlT88NtmR3w4s8rhSkBFtk3gOUXlHDjHJFo-X3KGbDxyS-dx3_AI7p9EKRVgy3g_WrS-3lBpQrMGu74YVEaHPsp7u9r3tv9GUijaZKKY03SvCsFOqCtnH3GbsIMukfajMgUtbleT048gak056EKGX717DbTjJNfqaRdM-AxDF174VdvgMPnydSMZ5c3Y-vIMq",
+    "/images/collins/after/collins-after-project-open-plan-2000x1000.webp",
 };
